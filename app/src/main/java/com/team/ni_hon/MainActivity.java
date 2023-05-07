16f16f0e243ee86b14e979665d0dd8b90096ad77
daniel-mora-moreno <daniel27mora96@gmail.com>
2023-05-07 19:39:23 +0200

    Small touchups after merge

--- app/src/main/java/com/team/ni_hon/MainActivity.java
+++ app/src/main/java/com/team/ni_hon/MainActivity.java
@@ -1,13 +1,10 @@
 package com.team.ni_hon;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,19 +14,6 @@ import com.team.ni_hon.recycler.LessonAdapter;
 import com.team.ni_hon.recycler.MyLinearLayoutManager;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainActivity extends AppCompatActivity {
