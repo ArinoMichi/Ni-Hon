f57f1c8aebc982bdbbf94cbde4f565675972e47d
daniel-mora-moreno <daniel27mora96@gmail.com>
2023-06-03 16:45:28 +0200

    added image loading to lessons

--- app/src/main/java/com/team/ni_hon/lesson_fragments/LessonFragment.java
+++ app/src/main/java/com/team/ni_hon/lesson_fragments/LessonFragment.java
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import com.team.ni_hon.LessonActivity;
 import com.team.ni_hon.R;
@@ -48,9 +51,14 @@ public class LessonFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_lesson, container, false);
 
         TextView text = view.findViewById(R.id.text);
-        String stringId = "lesson_" + lesson + "_" + page;
-        int resId = getResources().getIdentifier(stringId, "string", getActivity().getPackageName());
-        text.setText(getString(resId));
+        String textId = "lesson_" + lesson + "_" + page;
+        int textResId = getResources().getIdentifier(textId, "string", getActivity().getPackageName());
+        text.setText(getString(textResId));
+
+        ImageView image = view.findViewById(R.id.image);
+        String imageId = "lesson_" + lesson + "_" + page;
+        int imageResId = getResources().getIdentifier(imageId, "drawable", getActivity().getPackageName());
+        Glide.with(this).load(imageResId).into(image);
 
         return view;
     }
